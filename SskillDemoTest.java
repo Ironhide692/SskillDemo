ed841788803d12b0027e279c6689f10cc7550b44
Ironhide692 <77187616+Ironhide692@users.noreply.github.com>
2022-03-07 00:58:58 -0800

    fixed

--- SskillDemoTest.java
+++ SskillDemoTest.java
@@ -4,6 +4,6 @@ import org.junit.*;
 public class SskillDemoTest{
     @Test
     public void multiplying(){
-        assertEquals(27, SskillDemo.Multiply(3, 8)); //This will initially fail
+        assertEquals(27, SskillDemo.Multiply(3, 9)); //This will initially fail Let's fix this.
     }
 }
